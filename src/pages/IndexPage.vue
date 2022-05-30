54d27e395621bd93062d18ae3925ca5bdf70a0a1
YahorDanchanka <danchenko.dev@yandex.by>
2022-05-30 11:08:21 +0300

    Added searching on IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,5 +1,12 @@
 <template>
   <q-page padding>
+    <q-input
+      class="q-mb-sm"
+      label="Название темы"
+      v-model="searchQuery"
+      outlined
+      dense
+    />
     <TopicCard
       class="q-mb-sm"
       v-for="topic in topics"
@@ -10,12 +17,24 @@
 </template>
 
 <script lang="ts" setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
+import { filter } from 'lodash'
 import { useDictionaryStore } from 'stores/dictionary'
 import TopicCard from 'components/dictionary/TopicCard.vue'
 
 const dictionaryStore = useDictionaryStore()
 dictionaryStore.load()
 
-const topics = computed<string[]>(() => dictionaryStore.topics)
+const searchQuery = ref('')
+
+const topics = computed<string[]>(() =>
+  filter(
+    dictionaryStore.topics,
+    (topic) =>
+      topic
+        .toLowerCase()
+        .trim()
+        .indexOf(searchQuery.value.toLowerCase().trim()) !== -1
+  )
+)
 </script>
